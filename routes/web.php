0fbe93bc1c1ba45728b596dd187f2b45b13a04f3
Clauda <klodaalrakkad@gmail.com>
2026-05-11 19:37:10 +0300

    Add working Dockerfile for Laravel

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/print-qr/{store}', function (Store $store) {
     return view('print-qr', compact('store'));
 })->name('print.qr');
+
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/clear-all-cache', function() {
+    Artisan::call('optimize:clear');
+    return 'All caches cleared successfully!';
+});
